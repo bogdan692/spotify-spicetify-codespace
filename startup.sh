85a13710209762b9e876ba4e68a0a8de3072a479
bogdan692 <bogdanrus0601@gmail.com>
2026-02-25 02:46:24 +0100

    Update startup.sh for improved command usage

--- startup.sh
+++ startup.sh
@@ -4,20 +4,20 @@ echo "Оновлюємо Spicetify..."
 spicetify upgrade
 
 echo "Оновлюємо Spotify..."
-apt-get update && apt-get install -y spotify-client
+sudo apt update && sudo apt install -y spotify-client
 
 echo "Застосовуємо Spicetify..."
 spicetify apply
 
-echo "Створюємо віртуальний аудіо-пристрій..."
+echo "Створюємо віртуальний sink для PulseAudio..."
 pactl load-module module-null-sink sink_name=vsink
 
-echo "Запускаємо GUI..."
+echo "Запускаємо GUI через Xvfb та xfce4..."
 Xvfb :1 -screen 0 1024x768x24 &
 export DISPLAY=:1
 startxfce4 &
 
-echo "Запускаємо x11vnc..."
+echo "Запускаємо x11vnc для підключення через браузер..."
 x11vnc -display :1 -nopw -forever -shared -rfbport 5900 &
 
 echo "Запускаємо Spotify..."
